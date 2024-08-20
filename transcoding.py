21e12baaf0cbe36106570ff1d78eb61dbb1ce39f
viera97 <d.viera1997@gmail.com>
2024-08-20 19:45:22 -0400

    Removed ffmpeg and ffprobe
    For consuming less disk space the ffmpeg and ffprobe were removed, now
    they will be added as dependencies on the readme. Whoever in the
    releases it will be included in the file.

--- transcoding.py
+++ transcoding.py
@@ -1,18 +1,19 @@
 import ffmpeg
-import os
-import sys
 
-global ffmpeg_binary_path, ffprobe_binary_path
+#Release
+#import os
+#import sys
 
-#ffmpeg_binary_path = os.path.join(os.path.dirname(__file__), 'resources', 'ffmpeg')
-#ffprobe_binary_path = os.path.join(os.path.dirname(__file__), 'resources', 'ffprobe')
+#global ffmpeg_binary_path, ffprobe_binary_path
 
-ffmpeg_binary_path = os.path.join(sys._MEIPASS, 'ffmpeg')
-ffprobe_binary_path = os.path.join(sys._MEIPASS, 'ffprobe')
+#ffmpeg_binary_path = os.path.join(sys._MEIPASS, 'ffmpeg')
+#ffprobe_binary_path = os.path.join(sys._MEIPASS, 'ffprobe')
 
 def get_codec(input_file):
-    probe = ffmpeg.probe(input_file, ffprobe_binary_path)
-    #probe = ffmpeg.probe(input_file)
+    #Release
+    #probe = ffmpeg.probe(input_file, ffprobe_binary_path)
+
+    probe = ffmpeg.probe(input_file)
     video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
     
     if video_stream:
@@ -23,13 +24,7 @@ def get_codec(input_file):
 def transcode(input_file, output_file, vcodec="hevc", crf=18, acodec='aac'):
     stream = ffmpeg.input(input_file)
     stream = ffmpeg.output(stream, output_file, vcodec=vcodec, crf=crf, acodec=acodec, ab=128000)
-    ffmpeg.run(stream, quiet=False, cmd=[ffmpeg_binary_path])
 
-if __name__ == "__main__":
-    input_file = "/mnt/Work/Learn Organizar/Hablar con Poder/Introducción.mp4"
-    output_file = "/mnt/Work/Learn Organizar/Hablar con Poder/Introducción.mkv"
-
-    input_file_codec = get_codec(input_file)
-
-    if input_file_codec.lower() != "vp9" and input_file_codec.lower() != "hevc":
-            transcode(input_file, output_file)
+    #Release
+    #ffmpeg.run(stream, quiet=False, cmd=[ffmpeg_binary_path])
+    ffmpeg.run(stream, quiet=True)
